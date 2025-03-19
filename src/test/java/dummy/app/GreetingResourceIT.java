eb010e21ad9f4d3283d04dc8990fb4428da143d8
enzo_leveque <enzo_leveque@ens.univ-artois.fr>
2025-03-19 14:46:57 +0100

    :tada: - Initial commit.

---
+++ src/test/java/dummy/app/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package dummy.app;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
